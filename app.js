ce374691e55f53008a61028874f1ec077d2305fb
Mustafizur Rahman <mustafizur232@gmail.com>
2022-02-17 12:23:37 +0600

    done

--- app.js
+++ app.js
@@ -12,5 +12,37 @@ function getPin() {
 function generatePin() {
     const pin = getPin();
     console.log(pin);
+    document.getElementById('display-pin').value = pin;
 
+}
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const numbers = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(numbers)) {
+        if (numbers == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + numbers;
+        calcInput.value = newNumber;
+    }
+
+})
+function submitBtn() {
+    const generatePin = document.getElementById('display-pin').value;
+    const typedPin = document.getElementById('typed-numbers').value;
+    const succes = document.getElementById('pin-success');
+    const failed = document.getElementById('pin-failed');
+    if (generatePin == typedPin) {
+
+        succes.style.display = 'block';
+        failed.style.display = 'none';
+    }
+    else {
+        succes.style.display = 'none';
+        failed.style.display = 'block';
+
+    }
 }
